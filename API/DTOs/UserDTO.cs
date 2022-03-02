a5fc77e0daa40544762f0b586b956e9928ee5e6b
INoTime <niklas.lurse@icloud.com>
2022-03-02 16:42:13 +0100

    clear

--- API/DTOs/UserDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace API.DTOs;
-
-public class UserDTO
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-}
--- API/Storage.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Models;
-
-namespace API;
-
-public static class Storage
-{
-    public static List<User> Users = new List<User>();
-    public static List<Message> Messages = new List<Message>();
-}
