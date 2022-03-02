6091c69449062502945e9162bcc96b694296bb35
INoTime <niklas.lurse@icloud.com>
2022-03-02 14:09:56 +0100

    initial

---
+++ API/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class MessageController : Controller
+{
+    private readonly ILogger<MessageController> _logger;
+
+    public MessageController(ILogger<MessageController> logger)
+    {
+        _logger = logger;
+    }
+
+}
