ea908c620836e8297801e9e4afa49c99a0b36531
INoTime <niklas.lurse@icloud.com>
2022-03-09 16:40:24 +0100

    started to send message

--- Frontend/src/app/window/chat/chat.component.ts
+++ Frontend/src/app/window/chat/chat.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Chat} from "../../../models/chat";
+import {MessageService} from "../../../services/message.service";
+import {Message} from "../../../models/message";
+import {User} from "../../../models/user";
 
 @Component({
   selector: 'app-chat',
@@ -9,12 +12,27 @@ import {Chat} from "../../../models/chat";
 export class ChatComponent implements OnInit {
   @Input() chat: Chat | undefined;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
   getUserNamesFromChat() : string {
     return this.chat?.users.map(user => user.name).join(", ") as string;
+  }
+
+  sendMessage(text: string) {
+    const id: string = (JSON.parse(localStorage.getItem('user') ?? '') as User).id
+
+    let message: Message = {
+      text: text,
+      senderId: id,
+      receiverId: [
+        id
+      ],
+      toAllUsers: false,
+      timestamp: "0"
+    }
+    this.messageService.sendMessage(message).subscribe();
   }
 }
--- Frontend/src/services/message.service.ts
+++ Frontend/src/services/message.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../environments/environment";
+import {Message} from "../models/message";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  endpoint: string = "/Message";
+
+  constructor(private httpClient: HttpClient) { }
+
+  sendMessage(message: Message) : Observable<any> {
+    const url = `${environment.url}${this.endpoint}/SendMessage`;
+    return this.httpClient.post(url, message);
+  }
+
+  receiveMessages(id: string) : Observable<HttpResponse<Message>> {
+    const url = `${environment.url}${this.endpoint}/ReceiveMessages/${id}`;
+    return this.httpClient.get<Message>(url, {observe: 'response'});
+  }
 }
